8b61b9036dedbffcd99ce770ec8e4c69495db03f
yvbbrjdr <xyyqzy@hotmail.com>
2026-06-15 10:37:49 -0700

    feat(page): implement username retrieval and redirect to login if not found

--- apps/jukebox/src/app/page.tsx
+++ apps/jukebox/src/app/page.tsx
@@ -1,16 +1,28 @@
 "use client";
 
-import { useEffect } from "react";
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
 
 import { WsClient } from "../lib/ws-client";
 
 export default function Home() {
+  const router = useRouter();
   const wsClient = WsClient.getInstance();
+  const [username, setUsername] = useState<string>("");
+
+  useEffect(() => {
+    const savedUsername = localStorage.getItem("username");
+    if (savedUsername) {
+      setUsername(savedUsername);
+    } else {
+      router.push("/login");
+    }
+  }, []);
 
   useEffect(() => {
     wsClient.start();
     return () => wsClient.destroy();
   }, [wsClient]);
 
-  return <></>;
+  return <>{username}</>;
 }
